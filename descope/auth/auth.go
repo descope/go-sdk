6cba705e3ea979a2fe8b4df70fb79ac7d50a4acc
dorsha <dorsha@descope.com>
2022-07-05 20:10:40 +0300

    Add same site strict

--- descope/auth/auth.go
+++ descope/auth/auth.go
@@ -251,7 +251,7 @@ func createCookie(token *Token) *http.Cookie {
 			HttpOnly: true,
 			MaxAge:   int(maxAge),
 			Expires:  time.Unix(int64(expiration), 0),
-			SameSite: http.SameSiteNoneMode,
+			SameSite: http.SameSiteStrictMode,
 		}
 	}
 	return nil
