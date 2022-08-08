e9a82a3f181044fd44caa2d5c8b80bc4fa2eb03b
aviadl <aviad@descope.com>
2022-08-08 14:02:44 +0300

    Properly handle session validation

--- descope/auth/auth.go
+++ descope/auth/auth.go
@@ -181,14 +181,15 @@ func (auth *authenticationService) validateSession(sessionToken string, refreshT
 		logger.LogError("Cannot validate session, no public key available", err)
 		return false, nil, errors.NewNoPublicKeyError()
 	}
-	// parse refresh token, so we can get the refresh expiration as well
-	tToken, tErr := auth.validateJWT(refreshToken)
-	if tErr != nil {
-		logger.LogError("cannot validate refresh token, refresh expiration will not be available", err)
+	if err == nil {
+		// parse refresh token, so we can get the refresh expiration as well
+		tToken, tErr := auth.validateJWT(refreshToken)
+		if tErr != nil {
+			logger.LogError("cannot validate refresh token, refresh expiration will not be available", err)
+		} else {
+			token.RefreshExpiration = tToken.Expiration
+		}
 	} else {
-		token.RefreshExpiration = tToken.Expiration
-	}
-	if err != nil {
 		// check refresh token
 		if refreshToken == "" {
 			return false, nil, err
@@ -205,6 +206,12 @@ func (auth *authenticationService) validateSession(sessionToken string, refreshT
 		info, err := auth.generateAuthenticationInfo(httpResponse, options...)
 		if err != nil {
 			return false, nil, err
+		}
+		tToken, tErr := auth.validateJWT(refreshToken)
+		if tErr != nil {
+			logger.LogError("cannot validate refresh token for refresh response, refresh expiration will not be available", err)
+		} else {
+			info.SessionToken.RefreshExpiration = tToken.Expiration
 		}
 		return true, info.SessionToken, nil
 	}
@@ -227,24 +234,27 @@ func (auth *authenticationsBase) extractJWTResponse(bodyStr string) (*JWTRespons
 
 func (auth *authenticationsBase) collectJwts(jwt, rJwt string, tokens []*Token) ([]*Token, error) {
 	var err error
-	if len(jwt) == 0 && len(rJwt) == 0 {
-		return tokens, nil
-	}
-	token, err1 := auth.validateJWT(jwt)
-	if err1 == nil {
-		tokens = append(tokens, token)
-	} else {
-		err = err1
-	}
-	token2, err2 := auth.validateJWT(rJwt)
-	if err2 == nil {
-		if token != nil {
-			token.RefreshExpiration = token2.Expiration
+	var token *Token
+	if len(jwt) > 0 {
+		var err1 error
+		token, err1 = auth.validateJWT(jwt)
+		if err1 == nil {
+			tokens = append(tokens, token)
+		} else {
+			err = err1
 		}
-		token2.RefreshExpiration = token2.Expiration
-		tokens = append(tokens, token2)
-	} else {
-		err = err2
+	}
+	if len(rJwt) > 0 {
+		token2, err2 := auth.validateJWT(rJwt)
+		if err2 == nil {
+			if token != nil {
+				token.RefreshExpiration = token2.Expiration
+			}
+			token2.RefreshExpiration = token2.Expiration
+			tokens = append(tokens, token2)
+		} else {
+			err = err2
+		}
 	}
 
 	return tokens, err
