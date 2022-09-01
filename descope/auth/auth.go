267f1c93438b7841e85fb6be13b0bf3ed7febaf9
Slavik Markovich <s@descope.com>
2022-09-01 08:44:05 -0700

    Allow validation via either session or refresh token (#133)
    * Allow validation via either session or refresh token
    
    * fix tests

--- descope/auth/auth.go
+++ descope/auth/auth.go
@@ -135,13 +135,12 @@ func (auth *authenticationService) ValidateSessionWithOptions(request *http.Requ
 		return false, nil, errors.MissingProviderError
 	}
 
-	// Allow empty refresh token if all we want is to validate the session token
+	// Allow either empty session or refresh tokens if all we want is to validate the session token
 	sessionToken, refreshToken := provideTokens(request)
-	if sessionToken == "" {
+	if sessionToken == "" && refreshToken == "" {
 		logger.LogDebug("unable to find token from cookies")
 		return false, nil, nil
 	}
-
 	return auth.validateSession(sessionToken, refreshToken, false, options...)
 }
 
@@ -154,9 +153,9 @@ func (auth *authenticationService) RefreshSessionWithOptions(request *http.Reque
 		return false, nil, errors.MissingProviderError
 	}
 
-	// Allow empty refresh token if all we want is to validate the session token
+	// Allow either empty session or refresh tokens if all we want is to validate the session token
 	sessionToken, refreshToken := provideTokens(request)
-	if sessionToken == "" {
+	if sessionToken == "" && refreshToken == "" {
 		logger.LogDebug("unable to find token from cookies")
 		return false, nil, nil
 	}
@@ -194,27 +193,32 @@ func AuthenticationMiddleware(auth Authentication, onFailure func(http.ResponseW
 }
 
 func (auth *authenticationService) validateSession(sessionToken string, refreshToken string, forceRefresh bool, options ...Option) (bool, *Token, error) {
-	token, err := auth.validateJWT(sessionToken)
-	if sessionToken != "" && !auth.publicKeysProvider.publicKeyExists() {
+	// Make sure to try and validate either JWT because in the process we make sure we have the public keys
+	var token, tToken *Token
+	var err, tErr error
+	if sessionToken != "" {
+		token, err = auth.validateJWT(sessionToken)
+	}
+	if refreshToken != "" {
+		tToken, tErr = auth.validateJWT(refreshToken)
+	}
+	if !auth.publicKeysProvider.publicKeyExists() {
 		logger.LogError("Cannot validate session, no public key available", err)
 		return false, nil, errors.NewNoPublicKeyError()
 	}
-	if err == nil {
-		// parse refresh token, so we can get the refresh expiration as well
-		tToken, tErr := auth.validateJWT(refreshToken)
-		if tErr != nil {
-			logger.LogError("cannot validate refresh token, refresh expiration will not be available", tErr)
-		} else {
+	if err == nil && sessionToken != "" && refreshToken != "" {
+		if tErr == nil {
 			token.RefreshExpiration = tToken.Expiration
+		} else {
+			logger.LogError("cannot validate refresh token, refresh expiration will not be available", tErr)
 		}
 	}
-	if err != nil || forceRefresh {
+	if sessionToken == "" || err != nil || forceRefresh {
 		// check refresh token
 		if refreshToken == "" {
 			return false, nil, err
 		}
-		_, err := auth.validateJWT(refreshToken)
-		if ok, err := validateTokenError(err); !ok {
+		if ok, err := validateTokenError(tErr); !ok {
 			return false, nil, err
 		}
 		// auto-refresh session token
@@ -226,12 +230,8 @@ func (auth *authenticationService) validateSession(sessionToken string, refreshT
 		if err != nil {
 			return false, nil, err
 		}
-		tToken, tErr := auth.validateJWT(refreshToken)
-		if tErr != nil {
-			logger.LogError("cannot validate refresh token for refresh response, refresh expiration will not be available", err)
-		} else {
-			info.SessionToken.RefreshExpiration = tToken.Expiration
-		}
+		// No need to check for error again because validateTokenError will return false for any non-nil error
+		info.SessionToken.RefreshExpiration = tToken.Expiration
 		return true, info.SessionToken, nil
 	}
 
