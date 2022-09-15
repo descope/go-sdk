5f8fd86c78e7f7f7752ba4f5d5d393e7efe4298f
Gil Shapira <gshapira@gmail.com>
2022-09-15 14:46:27 +0300

    Add more tests

--- descope/auth/auth_test.go
+++ descope/auth/auth_test.go
@@ -607,16 +607,6 @@ func TestExtractJwtWithTenants(t *testing.T) {
 	assert.Len(t, m, 0)
 }
 
-func TestExchangeAccessKey(t *testing.T) {
-	a, err := newTestAuth(nil, DoOk(nil))
-	require.NoError(t, err)
-
-	ok, token, err := a.ExchangeAccessKey("foo")
-	require.True(t, ok)
-	require.NotNil(t, token)
-	require.NoError(t, err)
-}
-
 func BenchmarkValidateSession(b *testing.B) {
 	a, err := newTestAuth(nil, DoOk(nil))
 	require.NoError(b, err)
@@ -624,4 +614,45 @@ func BenchmarkValidateSession(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		_, _, _ = a.validateSession(jwtTokenValid, "", false)
 	}
+}
+
+func TestExchangeAccessKey(t *testing.T) {
+	a, err := newTestAuth(nil, DoOk(nil))
+	require.NoError(t, err)
+
+	ok, token, err := a.ExchangeAccessKey("foo")
+	require.NoError(t, err)
+	require.True(t, ok)
+	require.NotNil(t, token)
+}
+
+func TestExchangeAccessKeyBadRequest(t *testing.T) {
+	a, err := newTestAuth(nil, DoBadRequest(nil))
+	require.NoError(t, err)
+
+	ok, token, err := a.ExchangeAccessKey("foo")
+	require.ErrorIs(t, err, errors.UnauthorizedError)
+	require.False(t, ok)
+	require.Nil(t, token)
+}
+
+func TestExchangeAccessKeyEmptyResponse(t *testing.T) {
+	a, err := newTestAuth(nil, DoOkWithBody(nil, ""))
+	require.NoError(t, err)
+
+	ok, token, err := a.ExchangeAccessKey("foo")
+	require.ErrorIs(t, err, errors.InvalidAccessKeyResponse)
+	require.False(t, ok)
+	require.Nil(t, token)
+}
+
+func TestExchangeAccessKeyInvalidResponse(t *testing.T) {
+	expectedResponse := JWTResponse{}
+	a, err := newTestAuth(nil, DoOkWithBody(nil, expectedResponse))
+	require.NoError(t, err)
+
+	ok, token, err := a.ExchangeAccessKey("foo")
+	require.ErrorIs(t, err, errors.InvalidAccessKeyResponse)
+	require.False(t, ok)
+	require.Nil(t, token)
 }
