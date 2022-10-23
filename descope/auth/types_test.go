95c17813601734f57ee84d68dfe914a04cb8a174
aviadl <aviad@descope.com>
2022-10-24 00:47:19 +0300

    Fully cover types tests

--- descope/auth/types_test.go
+++ descope/auth/types_test.go
@@ -16,10 +16,19 @@ func TestIsStepup(t *testing.T) {
 }
 
 func TestAuthFactors(t *testing.T) {
-	to := &Token{Claims: map[string]interface{}{}}
+	to := &Token{}
+	assert.False(t, to.IsMFA())
+	to = &Token{Claims: map[string]interface{}{}}
 	assert.False(t, to.IsMFA())
 	to = &Token{Claims: map[string]interface{}{"amr": []interface{}{string(AuthFactorEmail)}}}
 	assert.False(t, to.IsMFA())
 	to = &Token{Claims: map[string]interface{}{"amr": []interface{}{string(AuthFactorEmail), string(AuthFactorWebauthn), string(AuthFactorMFA)}}}
 	assert.True(t, to.IsMFA())
+}
+
+func TestCustomClaims(t *testing.T) {
+	to := &Token{Claims: map[string]interface{}{}}
+	assert.Nil(t, to.CustomClaim("a"))
+	to = &Token{Claims: map[string]interface{}{"a": "b"}}
+	assert.EqualValues(t, "b", to.CustomClaim("a"))
 }
