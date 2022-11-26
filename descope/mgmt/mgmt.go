30a08db94818994ec77568e9236b2deee9931c1e
Gil Shapira <gshapira@gmail.com>
2022-11-27 00:03:51 +0200

    Move management key to config struct or environment variable (#170)
    * Update management API so that management keys are provided during DescopeClient initialization
    * Fix wrong log level when logging SDK errors

--- descope/mgmt/mgmt.go
+++ descope/mgmt/mgmt.go
@@ -2,15 +2,18 @@ package mgmt
 
 import (
 	"github.com/descope/go-sdk/descope/api"
+	"github.com/descope/go-sdk/descope/logger"
+	"github.com/descope/go-sdk/descope/utils"
 )
 
-type MgmtParams struct {
-	ProjectID string
+type ManagementParams struct {
+	ProjectID     string
+	ManagementKey string
 }
 
 type managementBase struct {
 	client *api.Client
-	conf   *MgmtParams
+	conf   *ManagementParams
 }
 
 type managementService struct {
@@ -21,7 +24,7 @@ type managementService struct {
 	sso    SSO
 }
 
-func NewManagement(conf MgmtParams, c *api.Client) *managementService {
+func NewManagement(conf ManagementParams, c *api.Client) *managementService {
 	base := managementBase{conf: &conf, client: c}
 	service := &managementService{managementBase: base}
 	service.tenant = &tenant{managementBase: base}
@@ -31,13 +34,22 @@ func NewManagement(conf MgmtParams, c *api.Client) *managementService {
 }
 
 func (mgmt *managementService) Tenant() Tenant {
+	mgmt.ensureManagementKey()
 	return mgmt.tenant
 }
 
 func (mgmt *managementService) User() User {
+	mgmt.ensureManagementKey()
 	return mgmt.user
 }
 
 func (mgmt *managementService) SSO() SSO {
+	mgmt.ensureManagementKey()
 	return mgmt.sso
+}
+
+func (mgmt *managementService) ensureManagementKey() {
+	if mgmt.conf.ManagementKey == "" {
+		logger.LogInfo("management key is missing, make sure to add it in the Config struct or the environment variable \"%s\"", utils.EnvironmentVariableManagementKey) // notest
+	}
 }
