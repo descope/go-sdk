0d79a7c03e6c55d590b09c35b7ba2d250a5c7128
Gil Shapira <gshapira@gmail.com>
2022-11-27 00:48:22 +0200

    Add management importusers sample

---
+++ examples/importusers/go.mod
@@ -0,0 +1,20 @@
+module github.com/descope/go-sdk/examples/importusers
+
+go 1.19
+
+replace github.com/descope/go-sdk => ../../
+
+require github.com/descope/go-sdk v0.0.0-20221114171656-dc158057b926
+
+require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc v1.0.4 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/jwx/v2 v2.0.6 // indirect
+	github.com/lestrrat-go/option v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	golang.org/x/exp v0.0.0-20220921023135-46d9e7742f1e // indirect
+)
---
+++ examples/importusers/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/descope/go-sdk/descope"
+	"github.com/descope/go-sdk/descope/mgmt"
+	"github.com/descope/go-sdk/descope/utils"
+)
+
+var descopeClient *descope.DescopeClient
+
+func prepare() (err error) {
+	if utils.GetProjectIDEnvVariable() == "" {
+		// the projectID can be found in the Project section of the admin console: https://app.descope.com/settings/project
+		return errors.New("the DESCOPE_PROJECT_ID environment variable must be set")
+	}
+	if utils.GetManagementKeyEnvVariable() == "" {
+		// generate a management key in the Company section of the admin console: https://app.descope.com/settings/company
+		return errors.New("the DESCOPE_MANAGEMENT_KEY environment variable must be set")
+	}
+	descopeClient, err = descope.NewDescopeClientWithConfig(&descope.Config{DescopeBaseURL: "https://api.sandbox.descope.com"})
+	return err
+}
+
+type TenantMapping struct {
+	TenantID string   `json:"tenantId"`
+	Roles    []string `json:"roles"`
+}
+
+type User struct {
+	Identifier  string          `json:"id"`
+	Email       string          `json:"email"`
+	Phone       string          `json:"phone"`
+	DisplayName string          `json:"displayName"`
+	Roles       []string        `json:"roles"`
+	Tenants     []TenantMapping `json:"tenants"`
+}
+
+type Data struct {
+	Users []*User `json:"users"`
+}
+
+func main() {
+	if len(os.Args) != 2 || len(os.Args[1]) == 0 {
+		fmt.Fprintln(os.Stderr, "Usage: importusers <jsonfile>")
+		os.Exit(1)
+	}
+
+	if err := prepare(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating DescopeClient:", err)
+		os.Exit(1)
+	}
+
+	bytes, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input file:", err)
+		os.Exit(1)
+	}
+
+	data := Data{}
+	if err := json.Unmarshal(bytes, &data); err != nil {
+		fmt.Fprintln(os.Stderr, "Error parsing input file:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Adding", len(data.Users), "users...")
+
+	for _, user := range data.Users {
+		fmt.Println("Adding user", user.Identifier, "("+user.DisplayName+")")
+
+		tenants := []mgmt.UserTenants{}
+		for _, curr := range user.Tenants {
+			tenants = append(tenants, mgmt.UserTenants{TenantID: curr.TenantID, Roles: curr.Roles})
+		}
+
+		err := descopeClient.Management.User().Create(user.Identifier, user.Email, user.Phone, user.DisplayName, user.Roles, tenants)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error adding user:", err)
+		}
+	}
+}
