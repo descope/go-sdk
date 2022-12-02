0dc80ed2a44726b3ceda93f9e0b6d314da1311d7
dorsha <dorsha@descope.com>
2022-12-02 23:51:57 +0200

    Fix lint

--- descope/tests/mocks/auth/authenticationmock_test.go
+++ descope/tests/mocks/auth/authenticationmock_test.go
@@ -4,10 +4,11 @@ import (
 	"testing"
 
 	"github.com/descope/go-sdk/descope/auth"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestManagementMock(t *testing.T) {
 	func(auth.Authentication) {
-
+		assert.True(t, true) // we just need to make sure the sdk is implemented
 	}(MockAuthentication{})
 }
--- descope/tests/mocks/mgmt/managementmock_test.go
+++ descope/tests/mocks/mgmt/managementmock_test.go
@@ -4,10 +4,11 @@ import (
 	"testing"
 
 	"github.com/descope/go-sdk/descope/mgmt"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestManagementMock(t *testing.T) {
 	func(mgmt.Management) {
-
+		assert.True(t, true) // we just need to make sure the sdk is implemented
 	}(MockManagement{})
 }
