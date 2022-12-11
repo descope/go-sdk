2037473e2b36abba933ea6c32bacf5ab3d1e1234
Aviad Lichtenstadt <aviad@descope.com>
2022-12-11 23:11:42 +0200

    Fix build script (#188)
    * Try to fix build script
    Make sure that it will fail if one of the builds in this repo fails
    
    * Make sample app fail
    See that build fails
    
    * Remove error in code
    So build will pass
    
    * Remove error in code So build will pass

--- scripts/build/ci/build_go.sh
+++ scripts/build/ci/build_go.sh
@@ -2,9 +2,21 @@
 
 echo 'Building main package..'
 go mod tidy && go mod vendor && go build -v ./...
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 echo 'Building gin package..'
 (cd descope/gin && go mod tidy && go mod vendor && go build)
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 echo 'Building mux web app example..'
 (cd examples/webapp && go mod tidy && go mod vendor && go build)
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 echo 'Building gin web app example..'
 (cd examples/ginwebapp && go mod tidy && go mod vendor && go build)
+if [ $? -ne 0 ]; then
+    exit 1
+fi
