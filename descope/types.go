e9bb0f42f81ac10e436f19a049ea602b5849ef84
Slavik Markovich <s@descope.com>
2023-05-27 15:34:06 -0700

    Forgot the base URL

--- descope/types.go
+++ descope/types.go
@@ -483,4 +483,5 @@ const (
 	EnvironmentVariableProjectID     = "DESCOPE_PROJECT_ID"
 	EnvironmentVariablePublicKey     = "DESCOPE_PUBLIC_KEY"
 	EnvironmentVariableManagementKey = "DESCOPE_MANAGEMENT_KEY"
+	EnvironmentVariableBaseURL       = "DESCOPE_BASE_URL"
 )
